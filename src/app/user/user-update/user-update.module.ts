8e4a5126283ec376bbbcdde00ccd235c9d54ce16
BenSegni <BBKSegni@hotmail.com>
2021-10-22 12:09:34 +0100

    moved shared to top level

--- src/app/user/user-update/user-update.module.ts
+++ src/app/user/user-update/user-update.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { UpdateUserComponent } from './update-user/update-user.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SharedElementsModule } from 'src/app/_common/_shared/shared-elements/shared-elements.module';
+import { SharedElementsModule } from 'src/app/_shared/shared-elements/shared-elements.module';
 
 //setting this as a module was not necessary, 
 //but just wanted to make a point of how we are able 
